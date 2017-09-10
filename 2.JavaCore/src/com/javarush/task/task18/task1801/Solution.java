f2515a54f616b12b64ec98a5026d6dc763210cce
neoMitya <neomitya@yandex.ru>
2017-09-10 20:26:47 +0300

    1722

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream fileInputStream = new FileInputStream(reader.readLine());
+
+        int max = 0;
+        while(fileInputStream.available() > 0){
+            int data = fileInputStream.read();
+            if(data > max){
+                max = data;
+            }
+        }
+        fileInputStream.close();
+        reader.close();
+        System.out.println(max);
+    }
+}
