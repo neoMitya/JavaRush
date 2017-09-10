0fc3a1f5402cfcd88937da26b4e146d706ae62a1
neoMitya <neomitya@yandex.ru>
2017-09-10 22:18:55 +0300

    1810

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task18.task1810;
+
+/* 
+DownloadException
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+
+
+        while(!file.isEmpty()){
+            FileInputStream fileInputStream = new FileInputStream(file);
+            if(fileInputStream.available() < 1000){
+                fileInputStream.close();
+                throw new DownloadException();
+            }else{
+                fileInputStream.close();
+                file = reader.readLine();
+            }
+        }
+
+
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
